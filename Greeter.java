871a7ce164e601e87c00848b12b5f72ec1824709
Student C Test <student+xavier-university-of-louisiana3@codio.com>
2020-08-24 17:15:40 +0000

    using name from comline

--- Greeter.java
+++ Greeter.java
@@ -1,5 +1,9 @@
 public class Greeter {
     public static void main(String[] args) {
-        System.out.println("Greetings, earthlings!");
+        if (args.length > 0) {
+            System.out.printf("Greetings, %s\n!",args[0]);
+        } else {
+            System.out.println("Greetings, earthling!");
+        }
     }
 }
