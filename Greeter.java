59d9bd5e338c780bd074ae9099d0f39b006feae9
Student C Test <student+xavier-university-of-louisiana3@codio.com>
2020-08-30 20:58:32 +0000

    swapped branches of condition

--- Greeter.java
+++ Greeter.java
@@ -1,9 +1,9 @@
 public class Greeter {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            System.out.printf("Greetings, %s\n!",args[0]);
-        } else {
+        if (args.length == 0) {
             System.out.println("Greetings, earthling!");
+        } else {
+            System.out.printf("Greetings, %s\n!",args[0]);
         }
     }
 }
