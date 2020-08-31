2800386e73c96b84caa2a3fd6518935f47b16110
Student C Test <student+xavier-university-of-louisiana3@codio.com>
2020-08-31 13:54:51 +0000

    greets multiple people

--- Greeter.java
+++ Greeter.java
@@ -1,9 +1,18 @@
+import java.util.*;
+
 public class Greeter {
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Greetings, earthling!");
         } else {
-            System.out.printf("Greetings, %s\n!",args[0]);
+            StringBuilder sb = new StringBuilder("Greetings");
+            for(int ndx = 0; ndx < args.length - 1; ndx++) {
+                sb.append(String.format(", %s", args[ndx]));
+            }
+            if (args.length > 1)
+                sb.append(", and");
+            sb.append(String.format(" %s!",args[args.length-1]));
+            System.out.println(sb.toString());
         }
     }
 }
